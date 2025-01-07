382e3ee3942ffb2350bb4a56afa4ca0f93295f3a
Graypbj <graypbj@gmail.com>
2025-01-07 14:31:59 -0700

    Added Point and Line classes with methods to draw them

--- main.py
+++ main.py
@@ -1,31 +1,11 @@
-from tkinter import Tk, BOTH, Canvas
+from graphics import Window, Point, Line
 
-class Window:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.__root = Tk()
-        self.__root.title("Hello there")
-        self.canvas = Canvas(self.__root, width=self.width, height=self.height)
-        self.canvas.pack(fill='both', expand=True)
-        self.window_running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
-    
-    def redraw(self):
-        self.__root.update_idletasks()
-        self.__root.update()
-    
-    def wait_for_close(self):
-        self.window_running = True
-        while self.window_running:
-            self.redraw()
-    
-    def close(self):
-        self.window_running = False
-        self.__root.destroy()
-    
 def main():
     win = Window(800, 600)
+    point1 = Point(0, 0)
+    point2 = Point(20, 20)
+    new_line = Line(point1, point2)
+    win.draw_line(new_line, "black")
     win.wait_for_close()
 
 if __name__ == "__main__":
