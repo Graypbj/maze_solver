e63f4cbf3912e88aba09438b750a65951b5bd488
Graypbj <graypbj@gmail.com>
2025-01-09 18:55:46 -0700

    Added maze class with methods to draw

---
+++ maze.py
@@ -0,0 +1,51 @@
+from cell import Cell
+import time
+
+class Maze:
+    def __init__(
+            self,
+            x1,
+            y1,
+            num_rows,
+            num_cols,
+            cell_size_x,
+            cell_size_y,
+            win,
+    ):
+        self._cells = []
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self._win = win
+
+        self._create_cells()
+    
+    def _create_cells(self):
+        for i in range(self.num_cols):
+            col_cells = []
+            for j in range(self.num_rows):
+                col_cells.append(Cell(self._win))
+            self._cells.append(col_cells)
+        for i in range(self.num_cols):
+            for j in range(self.num_rows):
+                self._draw_cell(i, j)
+    
+    def _draw_cell(self, i, j):
+        if self._win is None:
+            return
+        left_x = (i * self.cell_size_x) + self.x1
+        top_y = (j * self.cell_size_y) + self.y1
+        right_x = left_x + self.cell_size_x
+        bottom_y = top_y + self.cell_size_y
+        current_cell = self._cells[i][j]
+        current_cell.draw(left_x, top_y, right_x, bottom_y)
+        self._animate()
+
+    def _animate(self):
+        if self._win is None:
+            return
+        self._win.redraw()
+        time.sleep(0.05)
